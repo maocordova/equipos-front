25239edfb46b1ec399b8d85695ae74414d8e0cd6
Mao Cordova <mcq@teamq.biz>
2021-06-18 22:09:25 -0500

    feat: added old project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <Menubar :model="items">
+        <Button label="Salir" icon="pi pi-sign-out" />
+      </Menubar>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+body{
+  background-color: #17324f;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,3 +35,34 @@
   color: #42b983;
 }
 </style>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          {
+            label:'Home',
+            icon:'pi pi-fw pi-home',
+            url:'/'
+          },
+          {
+            label:'Clientes',
+            icon:'pi pi-fw pi-user',
+            url:'clientes'
+          },
+          {
+            label:'Equipos',
+            icon:'pi pi-fw pi-desktop',
+            url: 'equipos'
+          },
+          {
+            label:'Dispositivos',
+            icon:'pi pi-fw pi-print',
+            url: 'dispositivos'
+          }
+        ]
+      }
+    }
+  }
+</script>
